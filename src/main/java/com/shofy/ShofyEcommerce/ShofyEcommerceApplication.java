e59e273286b1918918a64f4593597d8d19fcded0
LeDangKhoa230101 <cho71167@gmail.com>
2024-05-25 14:31:04 +0700

    setup first

---
+++ src/main/java/com/shofy/ShofyEcommerce/ShofyEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.shofy.ShofyEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShofyEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShofyEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shofy/ShofyEcommerce/ShofyEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shofy.ShofyEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShofyEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
